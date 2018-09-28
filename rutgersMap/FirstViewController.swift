98650a63dd7e9648e1faf4ce59ed644193a387fd
Wahhaj Zahedi <wzahedi@gmail.com>
2018-09-27 21:43:38 -0400

    Initial Commit

---
+++ rutgersMap/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  rutgersMap
+//
+//  Created by Wahhaj Zahedi on 9/27/18.
+//  Copyright © 2018 Wahhaj Zahedi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ rutgersMap/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  rutgersMap
+//
+//  Created by Wahhaj Zahedi on 9/27/18.
+//  Copyright © 2018 Wahhaj Zahedi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
